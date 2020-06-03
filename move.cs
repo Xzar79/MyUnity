2fc184a16ab970efc0d0764911e62af371692a93
Xzar79 <57945703+Xzar79@users.noreply.github.com>
2020-06-03 13:10:36 +0900

    random Weapon PVP
    move 코드

---
+++ move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    Rigidbody rig;
+    public float speed = 8.0f;
+    public float jumpPower = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z );
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + jumpPower * Time.deltaTime, transform.position.z);
+        }
+    }
+}
